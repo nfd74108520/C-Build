3ae81ef478c0d7779b085de796ca0e5009a02140
George <nfd74108520@gmail.com>
2021-04-15 15:28:10 +0800

    apple

---
+++ Apple/product.js
@@ -0,0 +1,193 @@
+let product = {
+  iPhone: {
+    iPhone12: {
+      iPhone12: {
+        size: "iPhone 12",
+        standard: [
+          { capacity: 64, price: 26900 },
+          { capacity: 128, price: 28500 },
+          { capacity: 256, price: 32000 },
+        ],
+      },
+      iPhone12Mini: {
+        size: "iPhone 12 mini",
+        standard: [
+          { capacity: 64, price: 23900 },
+          { capacity: 128, price: 25500 },
+          { capacity: 256, price: 29000 },
+        ],
+      },
+      color: [
+        {
+          text: "白色",
+          value: "white",
+        },
+        {
+          text: "黑色",
+          value: "black",
+        },
+        {
+          text: "藍色",
+          value: "blue",
+        },
+        {
+          text: "綠色",
+          value: "green",
+        },
+        {
+          text: "(PRODUCT)RED",
+          value: "red",
+        },
+      ],
+    },
+    iPhone12Pro: {
+      iPhone12Pro: {
+        size: "iPhone 12 Pro",
+
+        standard: [
+          { capacity: 128, price: 33900 },
+          { capacity: 256, price: 37400 },
+          { capacity: 512, price: 44400 },
+        ],
+      },
+      iPhone12ProMax: {
+        size: "iPhone 12 Pro Max",
+        standard: [
+          { capacity: 128, price: 37900 },
+          { capacity: 256, price: 41400 },
+          { capacity: 512, price: 48400 },
+        ],
+      },
+      color: [
+        {
+          text: "石墨色",
+          value: "graphite",
+        },
+        {
+          text: "銀色",
+          value: "silver",
+        },
+        {
+          text: "金色",
+          value: "gold",
+        },
+        {
+          text: "太平洋藍色",
+          value: "blue",
+        },
+      ],
+    },
+    iPhoneSE: {
+      iPhoneSE: {
+        size: "iPhone SE",
+        standard: [
+          { capacity: 64, price: 13900 },
+          { capacity: 128, price: 15500 },
+          { capacity: 256, price: 19000 },
+        ],
+      },
+      color: [
+        {
+          text: "白色",
+          value: "white",
+        },
+        {
+          text: "黑色",
+          value: "black",
+        },
+        {
+          text: "(PRODUCT)RED",
+          value: "red",
+        },
+      ],
+    },
+    iPhone11: {
+      iPhone11: {
+        size: "iPhone 11",
+        standard: [
+          { capacity: 64, price: 19900 },
+          { capacity: 128, price: 21500 },
+          { capacity: 256, price: 25000 },
+        ],
+      },
+      color: [
+        {
+          text: "白色",
+          value: "white",
+        },
+        {
+          text: "黑色",
+          value: "black",
+        },
+        {
+          text: "綠色",
+          value: "green",
+        },
+        {
+          text: "黃色",
+          value: "yellow",
+        },
+        {
+          text: "紫色",
+          value: "purple",
+        },
+        {
+          text: "(PRODUCT)RED",
+          value: "red",
+        },
+      ],
+    },
+    iPhoneXR: {
+      iPhoneXR: {
+        size: "iPhone XR",
+        standard: [
+          { capacity: 64, price: 16900 },
+          { capacity: 128, price: 18500 },
+        ],
+      },
+      color: [
+        {
+          text: "白色",
+          value: "white",
+        },
+        {
+          text: "黑色",
+          value: "black",
+        },
+        {
+          text: "藍色",
+          value: "blue",
+        },
+        {
+          text: "黃色",
+          value: "yellow",
+        },
+        {
+          text: "珊瑚色",
+          value: "coral",
+        },
+        {
+          text: "(PRODUCT)RED",
+          value: "red",
+        },
+      ],
+    },
+  },
+  iPad: {
+    iPad: {},
+    iPadPro: {},
+    iPadAir: {},
+  },
+  Mac: {
+    MacBookAir: {},
+    MacBookPro13: {},
+    MacBookPro16: {},
+    iMac: {},
+  },
+  Watch: {
+    AppleWatchS6: {},
+    AppleWatchSE: {},
+    AppleWatchS3: {},
+    AppleWatchNike: {},
+  },
+};
