b893f5f8b04a2ba0f40d2bf441aa79c3e57627b3
George <nfd74108520@gmail.com>
2021-04-25 11:37:46 +0800

    todolist

--- todoList/todo.js
+++ todoList/todo.js
@@ -9,9 +9,9 @@ let displayMonth = document.getElementById("display-month");
 let displayDate = document.getElementById("display-date");
 
 let testdate = new Date(2021, 0, 1);
-console.log(testdate.getDay())
+// console.log(testdate.getDay())
 
-let yearCount = 2021;
+let yearCount = 2020;
 let monthCount = 0;
 let monthArray = [
   "一 月",
@@ -59,8 +59,24 @@ function changeMonth(select) {
 
 function createDate(date) {
   displayDate.innerHTML = "";
+
+  let firstTime = new Date(yearCount, monthCount, 1);
+  let firstDay = firstTime.getDay();
+
+  let lastTime = new Date(yearCount, monthCount, date);
+  let lastDay = lastTime.getDay();
+
   let row = document.createElement("div");
   row.setAttribute("class", "row");
+
+  for (let j = 0; j < firstDay; j++) {
+    let div = document.createElement("div");
+    div.addEventListener("click", todoList);
+    div.setAttribute("class", "col_7 py-3 border");
+
+    row.appendChild(div);
+  }
+
   for (let i = 0; i < date; i++) {
     let div = document.createElement("div");
     div.addEventListener("click", todoList);
@@ -72,6 +88,15 @@ function createDate(date) {
 
     row.appendChild(div);
   }
+
+  for (let k = 0; k < 6 - lastDay; k++) {
+    let div = document.createElement("div");
+    div.addEventListener("click", todoList);
+    div.setAttribute("class", "col_7 py-3 border");
+
+    row.appendChild(div);
+  }
+
   displayDate.appendChild(row);
 }
 
